d598de74148ded60e2ad6e66a4b078a8a17761c0
Trace_Recursion <lyhisso666@outlook.com>
2024-12-15 23:43:26 +0800

    A blank website

---
+++ docusaurus.config.js
@@ -0,0 +1,56 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+
+const config = {
+  title: 'Box64 for RISC-V Guide',
+  favicon: 'img/favicon.ico',
+  url: 'https://github.com',
+  baseUrl: '/box64-for-riscv-guide/',
+  organizationName: 'TraceRecursion',
+  projectName: 'box64-for-riscv-guide',
+  deploymentBranch: 'gh-pages',
+  onBrokenLinks: 'ignore',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      'classic',
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.js',
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+  themeConfig: ({
+      navbar: {
+        title: 'Box64 for RISC-V Guide',
+        items: [
+          {
+            href: 'https://github.com/ksco/box64-for-riscv-guide',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()}`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,37 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    Svg: () => null,
+  },
+];
+
+function Feature({Svg}) {
+  return (
+    <div className={clsx('col col--12')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3"></Heading>
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
